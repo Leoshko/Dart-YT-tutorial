ceeba2a405090bcc511e4f691e33e924b602696f
вероника леошко <veronica.leoshko@gmail.com>
2022-08-31 22:16:24 +0300

    Create a smart calculator

---
+++ Smarter_Calc.dart
@@ -0,0 +1,38 @@
+import 'dart:io';
+
+void main(List<String> args) {
+  double num1 = promptDouble();
+  double num2 = promptDouble();
+  String op = prompt("Enter an operation (+,-,/,*): ");
+
+  if (op == '+') {
+    print(num1 + num2);
+  } else if (op == '-') {
+    print(num1 - num2);
+  } else if (op == '/') {
+    print(num1 / num2);
+  } else if (op == '*') {
+    print(num1 * num2);
+  } else {
+    print("Invalid Operator");
+  }
+}
+
+
+
+
+
+
+
+
+String prompt(String promptText) {
+  print("The prompt is: $promptText");
+  String answer = stdin.readLineSync()!;
+  return answer;
+}
+
+double promptDouble() {
+  print("Enter a number: ");
+  double myNum = double.parse(stdin.readLineSync()!);
+  return myNum;
+}
